ff92b29d47f9d28801178ed158dcb7ccefa9e39e
martin <martonlineca@gmail.com>
2019-05-20 18:42:00 -0400

    first commit

---
+++ src/components/03232019components_different_format/Greet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//format 1
+//function Greet() {
+//	return <h1>Hello Vishwas</h1>
+//}
+
+//format 2.  format 1 and format 2 is the same result, but format 2 is ES6 format.
+const Greet = () => <h1>Hello Vishwas</h1>
+export default Greet
+
+//format 3, this is call name export
+//export const Greet = () => <h1>Hello Vishwas</h1>
